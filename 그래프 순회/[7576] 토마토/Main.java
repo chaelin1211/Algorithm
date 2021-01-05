4f9e6570b802349868481c6d690908e1fcbb64ca
chaelin1211 <cofls749@naver.com>
2021-01-05 22:01:26 +0900

    Make edge class and vertex class

--- 그래프 순회/[7576] 토마토/Main.java
+++ 그래프 순회/[7576] 토마토/Main.java
@@ -1,150 +1,190 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
+	// BFS 적용
+	static HashMap<Integer, ArrayList<edge>> edges = new HashMap<Integer, ArrayList<edge>>();
+	static HashMap<Integer, vertex> verts = new HashMap<Integer, vertex>();
 
-    public static void main(String arg[]) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String arg[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+		String str = br.readLine();
 
-        int M = Integer.parseInt(str.split(" ")[0]);
-        int N = Integer.parseInt(str.split(" ")[1]);
+		int M = Integer.parseInt(str.split(" ")[0]);
+		int N = Integer.parseInt(str.split(" ")[1]);
 
-        int numberOfZero = 0;
-        int index;
+		int index;
+		int numberOfZero = 0;
+		for (int i = 0; i < N; i++) {
+			str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				int tmp = str.charAt(j) - ('1' - 1);
+				index = i * M + j;
 
-        vertex[] verts = new vertex[N * M];
+				// 각 원소를 Vertex로 보고 Vertex 해쉬맵에 삽입
+				// 1인 경우만 삽입
+				if (tmp == 1)
+					verts.put(index, new vertex(index, tmp));
+				else if (tmp == 0)
+					numberOfZero++;
 
-        // 현재 익어있는 토마토~
-        ArrayList<Integer> vertexesOfOne = new ArrayList<Integer>();
+				// vertex가 -1이 아닐 경우 - edge를 가질 수 있음
+				if (tmp != -1) {
+					int ti = i, tj = j;
+					int tmpInd;
 
-        for (int i = 0; i < N; i++) {
-            str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                int tmp = Integer.parseInt(str.split(" ")[j]);
-                index = i * M + j;
+					// >>주위를 확인하여 삽입하는 과정
 
-                verts[index] = new vertex(index, tmp);
-                verts[index].incidentList = incidentList(i, j, N, M, verts);
+					// 위
+					if (i >= 1) {
+						ti--;
+						tmpInd = ti * M + tj;
+						if (verts.get(tmpInd)!=null) {
+							// 검색시 효율을 위해 Edge를 이중으로 저장
+							// a-b인 경우 (a,b), (b,a) 두 가지로 저장
+							if (edges.get(tmpInd) == null)
+								edges.put(tmpInd, new ArrayList<edge>());
+							if (edges.get(index) == null)
+								edges.put(index, new ArrayList<edge>());
+							edges.get(tmpInd).add(new edge(index, tmpInd));
+							edges.get(index).add(new edge(tmpInd, index));
+						}
+					}
 
-                if (tmp == 1) {
-                    vertexesOfOne.add(index);
-                } else if (tmp == 0) {
-                    numberOfZero++;
-                }
-            }
-        }
+					ti = i;
+					tj = j;
+					// 왼쪽
+					if (j >= 1) {
+						tj--;
+						tmpInd = ti * M + tj;
+						if (verts.get(tmpInd)!=null) {
+							// 검색시 효율을 위해 Edge를 이중으로 저장
+							// a-b인 경우 (a,b), (b,a) 두 가지로 저장
+							if (edges.get(tmpInd) == null)
+								edges.put(tmpInd, new ArrayList<edge>());
+							if (edges.get(index) == null)
+								edges.put(index, new ArrayList<edge>());
+							edges.get(tmpInd).add(new edge(index, tmpInd));
+							edges.get(index).add(new edge(tmpInd, index));
+						}
+					}
+				}
+			}
+		}
+		System.out.println(func(numberOfZero));
+	}
 
-        System.out.println(func(verts, vertexesOfOne, numberOfZero));
-    }
+	public static int func(int numberOfZero) {
+		int ind = 0;
+		int count = 0;
 
-    public static int func(vertex[] verts, ArrayList<Integer> vertexesOfOne, int numberOfZero) {
-        int count = 0;
-        int ind = 0;
+		ArrayList<vertex> tmpVerListA = new ArrayList<vertex>();
+		ArrayList<vertex> tmpVerListB = new ArrayList<vertex>();
 
-        if (numberOfZero == 0) {
-            return count;
-        }
-        // 한 레벨을 한 리스트에 넣고 다음 레벨은 다른 리스트에 넣어 레벨 간의 구별을 해주고자 함
-        // 두 리스트를 번갈아 사용하고자 하였고 쉽게 이용하기 위해 리스트를 배열로 다시 구성
-        ArrayList<Integer>[] ListToggle = new ArrayList[2];
-        ListToggle[0] = vertexesOfOne;
-        ListToggle[1] = new ArrayList<Integer>();
+		// 한 레벨을 한 리스트에 넣고 다음 레벨은 다른 리스트에 넣어 레벨 간의 구별을 해주고자 함
+		// 두 리스트를 번갈아 사용하고자 하였고 쉽게 이용하기 위해 리스트를 배열로 다시 구성
+		ArrayList<vertex>[] ListToggle = new ArrayList[2];
+		ListToggle[0] = tmpVerListA;
+		ListToggle[1] = tmpVerListB;
 
-        while (!ListToggle[ind].isEmpty() && numberOfZero > 0) {
-            vertex nowVer = verts[(int) ListToggle[ind].remove(0)];
+		vertex ver = verts.get(0);
 
-            int[] incidentList = nowVer.incidentList;
+		// 현재 익은 토마토들
+		ListToggle[0].addAll(verts.values());
 
-            if (incidentList.length == 0) {
-                continue;
-            }
+		while (!ListToggle[ind].isEmpty()&&numberOfZero>0) {
+			ver = ListToggle[ind].remove(0);
 
-            ArrayList<Integer> tmpList = new ArrayList<Integer>();
-            for (int i : incidentList) {
-                vertex inciVer = verts[i];
+			ArrayList<edge> tmpList = edges.get(ver.index);
 
-                if (inciVer.number != 0)
-                    continue;
-                // 주위의 토마토가 익는 중~
-                inciVer.number = 1;
-                numberOfZero--;
+			// 현재 Vertex의 연결 Edge를 확인해 반대편 Vertex를 다른 리스트에 삽입
+			while (!tmpList.isEmpty()) {
+				edge edg = tmpList.remove(0);
 
-                tmpList.add(inciVer.index);
-            }
+				if (edg.flag != 1) {
+					// 반대편 Vertex 리턴 - 오류 시 -1
+					int oppsiteInd = edg.otherPoint(ver.index);
 
-            if (ind == 0) {
-            	if(ListToggle[1].size()==0) count++;
-                ListToggle[1].addAll(tmpList);
-            } else if (ind == 1) {
-            	if(ListToggle[0].size()==0) count++;
-                ListToggle[0].addAll(tmpList);
-            }
-            
-            if (ListToggle[ind].isEmpty()) {
-                if (ind == 0)
-                    ind = 1;
-                else
-                    ind = 0;
-            }
-        }
+					if (oppsiteInd != -1) {
+						vertex tmpVer = verts.get(oppsiteInd);
 
-        if (numberOfZero > 0) {
-            count = -1;
-        }
-        return count;
-    }
+						// 해당 Edge는 트리 간선
+						edg.flag = 1;
 
-    public static int[] incidentList(int i, int j, int N, int M, vertex[] verts) {
-    	int[] tmp = new int[4];
-    	int k=0;
-        int x;
-
-        // 상
-        if (i > 0) {
-            x = (i - 1) * M + j;
-            tmp[k++]=x;
-        }
-
-        // 하
-        if (i < N - 1) {
-            x = (i + 1) * M + j;
-            tmp[k++]=x;
-        }
-
-        // 좌
-        if (j > 0) {
-            x = i * M + (j - 1);
-            tmp[k++]=x;
-        }
-
-        // 우
-        if (j < M - 1) {
-            x = i * M + (j + 1);
-            tmp[k++]=x;
-        }
-        return tmp;
-    }
+						if (tmpVer.number == 0) {
+							numberOfZero--;
+							tmpVer.number = 1;
+							if (ind == 0)
+								ListToggle[1].add(tmpVer);
+							else
+								ListToggle[0].add(tmpVer);
+						} else {
+							// 해당 Edge는 교차 간선
+							// 현재 문제 상 필요한 로직은 아니므로 구현하지 않음
+						}
+					}
+				}
+			}
+			if (ListToggle[ind].isEmpty()) {
+				count++;
+				if (ind == 0)
+					ind = 1;
+				else
+					ind = 0;
+			}
+		}
+		
+		if(numberOfZero>0) {
+			return -1;
+		}
+		return count;
+	}
 }
 
 class vertex {
-    int index;
-    int number;
-	int[] incidentList;
+	int index;
+	int number;
 
-    public vertex() {
-        this.index = 0;
-        this.number = 0;
-    }
+	public vertex() {
+		this.index = 0;
+		this.number = 0;
+	}
 
-    public vertex(int index, int number) {
-        this.index = index;
-        this.number = number;
-    }
+	public vertex(int index, int number) {
+		this.index = index;
+		this.number = number;
+	}
+}
+
+class edge {
+	int s, e;
+	int flag;
+	// 0: 초기 상태
+	// 1: 트리 간선
+
+	public edge() {
+		this.s = 0;
+		this.e = 0;
+		this.flag = 0;
+	}
+
+	public edge(int s, int e) {
+		this.s = s;
+		this.e = e;
+		this.flag = 0;
+	}
+
+	public int otherPoint(int a) {
+		if (a == this.s) {
+			return this.e;
+		} else if (a == this.e) {
+			return this.s;
+		} else {
+			return -1;
+		}
+	}
 }
