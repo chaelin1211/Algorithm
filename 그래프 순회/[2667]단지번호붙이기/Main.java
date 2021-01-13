1fcbb1ef90ed72339e774b83132f52bc9eba77a8
chaelin1211 <cofls749@naver.com>
2021-01-13 23:58:48 +0900

    Add print output

--- 그래프 순회/[2667]단지번호붙이기/Main.java
+++ 그래프 순회/[2667]단지번호붙이기/Main.java
@@ -8,66 +8,77 @@ public class Main {
 	public static void main(String arg[]) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-        String[][] arr = new String[N][N];
-        vertex[][] verList = new vertex[N][N];
-        HashMap<Integer, ArrayList<vertex>> belongArr = new HashMap<Integer, ArrayList<vertex>>();
+		String[][] arr = new String[N][N];
+		vertex[][] verList = new vertex[N][N];
+		HashMap<Integer, ArrayList<vertex>> belongArr = new HashMap<Integer, ArrayList<vertex>>();
 
-        int idx = 1;
+		int idx = 1;
 
-        // input
-        for (int i = 0; i < N; i++) {
-            String[] tmp = br.readLine().split("");
-            arr[i] = tmp;
-            for (int j = 0; j < N; j++) {
-                if (arr[i][j].equals("1")) {
-                	//belongArr.put(idx++, new ArrayList<vertex>());
-                    verList[i][j] = new vertex(i,j);
-                    verList[i][j].index = idx;
+		// input
+		for (int i = 0; i < N; i++) {
+			String[] tmp = br.readLine().split("");
+			arr[i] = tmp;
+			for (int j = 0; j < N; j++) {
+				if (arr[i][j].equals("1")) {
+					// belongArr.put(idx++, new ArrayList<vertex>());
+					verList[i][j] = new vertex(i, j);
+					verList[i][j].index = idx;
 
-                    int minInd = verList[i][j].index;
-                    int flag = 1;
-                    
-                    if(i>0 && verList[i-1][j]!=null){
-                        if(minInd>verList[i-1][j].index){
-                            minInd = verList[i-1][j].index;
-                            flag = 0;
-                        }else if(minInd<verList[i-1][j].index){
-                        	belongArr.get(minInd).addAll(belongArr.get(verList[i-1][j].index));
-                            belongArr.remove(verList[i-1][j].index);
-                        	verList[i-1][j].index = minInd;
-                        }
-                    }
-                    if(j>0 && verList[i][j-1]!=null){
-                        if(minInd>verList[i][j-1].index){
-                            minInd = verList[i][j-1].index;
-                            flag = 0;
-                        }else if(minInd<verList[i][j-1].index){
-                        	belongArr.get(minInd).addAll(belongArr.get(verList[i][j-1].index));
-                            belongArr.remove(verList[i][j-1].index);
-                        	verList[i][j-1].index = minInd;
-                        }
-                    }
-                    idx += flag;
-                    verList[i][j].index = minInd;
-                    if(belongArr.get(minInd)== null)
-                    	belongArr.put(minInd, new ArrayList<vertex>());
-                    belongArr.get(minInd).add(verList[i][j]);
-                }
-            }
-        }
-    }
+					int minInd = verList[i][j].index;
+					int flag = 1;
+
+					if (i > 0 && verList[i - 1][j] != null) {
+						if (minInd > verList[i - 1][j].index) {
+							minInd = verList[i - 1][j].index;
+							flag = 0;
+						}
+					}
+					if (j > 0 && verList[i][j - 1] != null) {
+						if (minInd > verList[i][j - 1].index) {
+							minInd = verList[i][j - 1].index;
+							flag = 0;
+						}
+					}
+
+					if (i > 0 && verList[i - 1][j] != null) {
+						if (minInd < verList[i - 1][j].index) {
+							belongArr.get(minInd).addAll(belongArr.get(verList[i - 1][j].index));
+							belongArr.remove(verList[i - 1][j].index);
+							verList[i - 1][j].index = minInd;
+						}
+					}
+					if (j > 0 && verList[i][j - 1] != null) {
+						if (minInd < verList[i][j - 1].index) {
+							belongArr.get(minInd).addAll(belongArr.get(verList[i][j - 1].index));
+							belongArr.remove(verList[i][j - 1].index);
+							verList[i][j - 1].index = minInd;
+						}
+					}
+					idx += flag;
+					verList[i][j].index = minInd;
+					if (belongArr.get(minInd) == null)
+						belongArr.put(minInd, new ArrayList<vertex>());
+					belongArr.get(minInd).add(verList[i][j]);
+				}
+			}
+		}
+		System.out.println(belongArr.size());
+		for(ArrayList<vertex> a:belongArr.values()) {
+			System.out.println(a.size());
+		}
+	}
 }
 
 class vertex {
-    int i, j;
-    int index;
-    ArrayList<vertex> belongArray;
+	int i, j;
+	int index;
+	ArrayList<vertex> belongArray;
 
-    public vertex(int i, int j) {
-        this.i = i;
-        this.j = j;
-        this.index = 0;
-    }
+	public vertex(int i, int j) {
+		this.i = i;
+		this.j = j;
+		this.index = 0;
+	}
 }
