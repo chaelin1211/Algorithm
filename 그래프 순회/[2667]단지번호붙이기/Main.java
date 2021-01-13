c14f468520469546e30e70a10e2f1cff066b27d2
chaelin1211 <cofls749@naver.com>
2021-01-14 01:12:46 +0900

    Create edge class

--- 그래프 순회/[2667]단지번호붙이기/Main.java
+++ 그래프 순회/[2667]단지번호붙이기/Main.java
@@ -5,80 +5,106 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
-	public static void main(String arg[]) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+	public static void main(String arg[]) {
+		try {
+			inputFunction();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void inputFunction() throws NumberFormatException, IOException {
 		int N = Integer.parseInt(br.readLine());
 
-		String[][] arr = new String[N][N];
-		vertex[][] verList = new vertex[N][N];
-		HashMap<Integer, ArrayList<vertex>> belongArr = new HashMap<Integer, ArrayList<vertex>>();
-
-		int idx = 1;
+		vertex[][] verTable = new vertex[N][N];
+		ArrayList<vertex> verList = new ArrayList<vertex>();
 
 		// input
 		for (int i = 0; i < N; i++) {
 			String[] tmp = br.readLine().split("");
-			arr[i] = tmp;
 			for (int j = 0; j < N; j++) {
-				if (arr[i][j].equals("1")) {
-					// belongArr.put(idx++, new ArrayList<vertex>());
-					verList[i][j] = new vertex(i, j);
-					verList[i][j].index = idx;
+				int a = Integer.parseInt(tmp[j]);
+				if (a == 1) {
+					vertex newVer = new vertex(i, j);
 
-					int minInd = verList[i][j].index;
-					int flag = 1;
+					verList.add(newVer);
+					verTable[i][j] = newVer;
 
-					if (i > 0 && verList[i - 1][j] != null) {
-						if (minInd > verList[i - 1][j].index) {
-							minInd = verList[i - 1][j].index;
-							flag = 0;
-						}
-					}
-					if (j > 0 && verList[i][j - 1] != null) {
-						if (minInd > verList[i][j - 1].index) {
-							minInd = verList[i][j - 1].index;
-							flag = 0;
-						}
-					}
-
-					if (i > 0 && verList[i - 1][j] != null) {
-						if (minInd < verList[i - 1][j].index) {
-							belongArr.get(minInd).addAll(belongArr.get(verList[i - 1][j].index));
-							belongArr.remove(verList[i - 1][j].index);
-							verList[i - 1][j].index = minInd;
-						}
-					}
-					if (j > 0 && verList[i][j - 1] != null) {
-						if (minInd < verList[i][j - 1].index) {
-							belongArr.get(minInd).addAll(belongArr.get(verList[i][j - 1].index));
-							belongArr.remove(verList[i][j - 1].index);
-							verList[i][j - 1].index = minInd;
-						}
-					}
-					idx += flag;
-					verList[i][j].index = minInd;
-					if (belongArr.get(minInd) == null)
-						belongArr.put(minInd, new ArrayList<vertex>());
-					belongArr.get(minInd).add(verList[i][j]);
+					inputInciVerToList(i, j, verTable, newVer, verList);
 				}
 			}
 		}
+	}
+
+	public static void outputFunction(HashMap<Integer, ArrayList<vertex>> belongArr) {
 		System.out.println(belongArr.size());
-		for(ArrayList<vertex> a:belongArr.values()) {
+		for (ArrayList<vertex> a : belongArr.values()) {
 			System.out.println(a.size());
 		}
+	}
+
+	public static void inputInciVerToList(int i, int j, vertex[][] arr, vertex ver, ArrayList<vertex> verList) {
+		// 주위의 1인 vertex를 찾아 삽입
+		vertex inciVer;
+		if (i > 0 && arr[i - 1][j] != null) {
+			inciVer = arr[i - 1][j];
+
+			ver.addEdgeList(new edge(ver, inciVer));
+			inciVer.addEdgeList(new edge(inciVer, ver));
+		}
+		if (j > 0 && arr[i][j - 1] != null) {
+			inciVer = new vertex(i, j - 1);
+
+			ver.addEdgeList(new edge(ver, inciVer));
+			inciVer.addEdgeList(new edge(inciVer, ver));
+		}
+
 	}
 }
 
 class vertex {
 	int i, j;
-	int index;
-	ArrayList<vertex> belongArray;
+	boolean flag;
+	// 방문 전 false
+	// 방문 후 true
+	ArrayList<edge> inciList;
+
+	public vertex() {
+		this.inciList = new ArrayList<edge>();
+		this.flag = false;
+	}
 
 	public vertex(int i, int j) {
 		this.i = i;
 		this.j = j;
-		this.index = 0;
+		this.inciList = new ArrayList<edge>();
+		this.flag = false;
+	}
+
+	public void addEdgeList(edge edg) {
+		this.inciList.add(edg);
+	}
+}
+
+class edge {
+	vertex s, e;
+	boolean flag;
+
+	// 방문 전 false
+	// 방문 후 true
+	public edge() {
+		this.s = null;
+		this.e = null;
+		flag = false;
+	}
+
+	public edge(vertex s, vertex e) {
+		this.s = s;
+		this.e = e;
+		flag = false;
 	}
 }
