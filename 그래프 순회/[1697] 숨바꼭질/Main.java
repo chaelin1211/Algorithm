b67f6cbb5b6d094bd92bc8aa379e082789dfae39
chaelin1211 <cofls749@naver.com>
2021-01-15 04:02:18 +0900

    Create new folder and file [1697]

---
+++ 그래프 순회/[1697] 숨바꼭질/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String tmp = br.readLine();
+        int N = Integer.parseInt(tmp.split(" ")[0]);
+        int K = Integer.parseInt(tmp.split(" ")[1]);
+    }
+}
