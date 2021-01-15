20d05db83b8ec2bec49de160d6a4c97243b22b0f
chaelin1211 <cofls749@naver.com>
2021-01-16 04:59:39 +0900

    Add comparator method to schedule class

--- Greedy Algorithm/[1931] 회의실 배정/Main.java
+++ Greedy Algorithm/[1931] 회의실 배정/Main.java
@@ -1,53 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
-        int N = Integer.parseInt(input);
+		String input = br.readLine();
+		int N = Integer.parseInt(input);
 
-        HashMap<Integer, ArrayList<schedule>> hash = new HashMap<Integer, ArrayList<schedule>>();
+		HashMap<Integer, ArrayList<schedule>> hash = new HashMap<Integer, ArrayList<schedule>>();
 
-        for (int i = 0; i < N; i++) {
-            input = br.readLine();
-            int a, b;
-            a = Integer.parseInt(input.split(" ")[0]);
-            b = Integer.parseInt(input.split(" ")[1]);
-            if(hash.get(a)==null){
-                hash.put(a, new ArrayList<schedule>());
-            }
-            hash.get(a).add(new schedule(a, b));
-        }
-        System.out.println(function(hash));
-    }
+		for (int i = 0; i < N; i++) {
+			input = br.readLine();
+			int a, b;
 
-    public static int function(HashMap<Integer, ArrayList<schedule>> hash) {
-        int i = A[A.length - 1];
-        int count = 0;
+			a = Integer.parseInt(input.split(" ")[0]);
+			b = Integer.parseInt(input.split(" ")[1]);
 
-        while (K != 0) {
-            if (K < A[i]) {
-                i--;
-                continue;
-            }
-            K -= A[i];
-            count++;
-        }
-        return count;
-    }
+			if (hash.get(b - a) == null) {
+				hash.put(b - a, new ArrayList<schedule>());
+			}
+			hash.get(b - a).add(new schedule(a, b));
+		}
+		System.out.println(function(hash));
+	}
+
+	public static int function(HashMap<Integer, ArrayList<schedule>> hash) {
+	
+	}
 }
 
-class schedule {
-    int sTime;
-    int eTime;
-    int mTime; // meeting time
+class schedule implements Comparable<schedule> {
+	private int sTime;
+	private int eTime;
 
-    public schedule(int sTime, int eTime) {
-        this.sTime = sTime;
-        this.eTime = eTime;
-        this.mTime = this.eTime - this.sTime;
-    }
+	public schedule(int sTime, int eTime) {
+		this.sTime = sTime;
+		this.eTime = eTime;
+	}
+
+	public int getStime() {
+		return this.sTime;
+	}
+
+	public int getEtime() {
+		return this.eTime;
+	}
+
+	public int compareTo(schedule s) {
+		if (sTime > s.sTime)
+			return 1;
+		else if (sTime == s.sTime)
+			return 0;
+		else
+			return -1;
+	}
 }
