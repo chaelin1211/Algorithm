1c7f2bd5fe1687efd09922992c2a1d420af42731
chaelin1211 <cofls749@naver.com>
2021-01-15 13:33:49 +0900

    반복문을 이용한 누적합으로 해답을 구함

--- Greedy Algorithm/[11399] ATM/Main.java
+++ Greedy Algorithm/[11399] ATM/Main.java
@@ -1,16 +1,36 @@
 import java.io.BufferedReader;
-
-import jdk.internal.org.jline.utils.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-        String P = br.readLine();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(P.split(" ")[i]);
-        }
-    }
+		String P = br.readLine();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+
+		for (int i = 0; i < N; i++) {
+			arr.add(Integer.parseInt(P.split(" ")[i]));
+		}
+
+		System.out.println(function(N, arr));
+	}
+
+	public static int function(int N, ArrayList<Integer> arr) {
+		int nowTime = 0;
+		int sum = 0;
+		int beforeWait = 0;
+		arr.sort(null);
+		while (!arr.isEmpty()) {
+			nowTime = arr.remove(0);
+			
+			sum += beforeWait + nowTime;
+			
+			beforeWait += nowTime;
+		}
+
+		return sum;
+	}
 }
