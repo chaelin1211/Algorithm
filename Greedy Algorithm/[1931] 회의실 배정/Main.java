3d1630cfada7deb7ea4dee111d8723347c11a4de
chaelin1211 <cofls749@naver.com>
2021-01-16 06:00:17 +0900

    Compare the number of overlapping schedules

--- Greedy Algorithm/[1931] 회의실 배정/Main.java
+++ Greedy Algorithm/[1931] 회의실 배정/Main.java
@@ -12,7 +12,8 @@ public class Main {
 		String input = br.readLine();
 		int N = Integer.parseInt(input);
 
-		HashMap<Integer, ArrayList<schedule>> hash = new HashMap<Integer, ArrayList<schedule>>();
+		ArrayList<schedule> scheduleArr = new ArrayList<schedule>();
+		HashMap<Integer, Integer> numOfSchedule = new HashMap<Integer, Integer>();
 
 		for (int i = 0; i < N; i++) {
 			input = br.readLine();
@@ -21,16 +22,67 @@ public class Main {
 			a = Integer.parseInt(input.split(" ")[0]);
 			b = Integer.parseInt(input.split(" ")[1]);
 
-			if (hash.get(b - a) == null) {
-				hash.put(b - a, new ArrayList<schedule>());
+			scheduleArr.add(new schedule(a, b));
+			if (numOfSchedule.get(a) == null)
+				numOfSchedule.put(a, 1);
+			else {
+				int x = numOfSchedule.get(a);
+				numOfSchedule.put(a, x + 1);
 			}
-			hash.get(b - a).add(new schedule(a, b));
 		}
-		System.out.println(function(hash));
+		System.out.println(function(scheduleArr, numOfSchedule));
 	}
 
-	public static int function(HashMap<Integer, ArrayList<schedule>> hash) {
-	
+	public static int function(ArrayList<schedule> arr, HashMap<Integer, Integer> numOfSchedule) {
+		int count = 0;
+
+		if (arr.isEmpty())
+			return 0;
+
+		Collections.sort(arr);
+		schedule beforeSchedule = arr.remove(0);
+		int numOfImpossibleScheduleBefore = numOfImpossibleSchedule(beforeSchedule, numOfSchedule);
+		schedule nowSchedule;
+		while (!arr.isEmpty()) {
+			nowSchedule = arr.remove(0);
+
+			int numOfImpossibleScheduleNow = numOfImpossibleSchedule(nowSchedule, numOfSchedule);
+
+			if (beforeSchedule.getEtime() < nowSchedule.getStime()) {
+				count++;
+				beforeSchedule = nowSchedule;
+				numOfImpossibleScheduleBefore = numOfImpossibleScheduleNow;
+				if(arr.isEmpty()) count++;
+				continue;
+			}
+
+			numOfImpossibleScheduleNow++;
+
+			if (numOfImpossibleScheduleBefore > numOfImpossibleScheduleNow) {
+				beforeSchedule = nowSchedule;
+				numOfImpossibleScheduleBefore = --numOfImpossibleScheduleNow;
+			}else {
+				int tmp = nowSchedule.getStime();
+				numOfSchedule.put(tmp, numOfSchedule.get(tmp)-1);
+
+				numOfImpossibleScheduleBefore--;
+			}
+		}
+		return count;
+	}
+
+	public static int numOfImpossibleSchedule(schedule sch, HashMap<Integer, Integer> numOfSchedule) {
+		int count = 0;
+		int s = sch.getStime();
+		int e = sch.getEtime();
+
+		for (int i = s ; i < e; i++) {
+			if (numOfSchedule.get(i) == null)
+				continue;
+			count += numOfSchedule.get(i);
+		}
+
+		return count-1;	//현재 스케줄도 개수에 포함되니 제외해줌
 	}
 }
 
