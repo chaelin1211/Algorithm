e0d1e9d04b1b9033a00e56844c3c9ee564b92cd8
chaelin1211 <cofls749@naver.com>
2021-01-18 11:16:03 +0900

    Create Rank class

--- Greedy Algorithm/[1946] 신입사원/Main.java
+++ Greedy Algorithm/[1946] 신입사원/Main.java
@@ -1,5 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
-    public static void main(String[] args) {
-        
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+		int T = Integer.parseInt(input.split(" ")[0]);
+		for (int i = 0; i < T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			ArrayList<Rank> rank = new ArrayList<Rank>();
+			for (int j = 0; j < N; j++) {
+				input = br.readLine();
+				int a = Integer.parseInt(input.split(" ")[0]);
+				int b = Integer.parseInt(input.split(" ")[1]);
+				rank.add(new Rank(a, b));
+			}
+			System.out.println(function(rank));
+		}
+	}
+
+	public static int function(ArrayList<Rank> ranks) {
+		// 순위대로 정렬
+		int count = 1;
+		Collections.sort(ranks);
+		int minRank = ranks.remove(0).r2;
+		for (Rank p : ranks) {
+			if (minRank > p.r2) {
+				count++;
+				minRank = p.r2;
+			}
+		}
+
+		return count;
+	}
+}
+
+class Rank implements Comparable<Rank> {
+	int r1, r2;
+
+	public Rank(int r1, int r2) {
+		this.r1 = r1;
+		this.r2 = r2;
+	}
+
+	public int compareTo(Rank r) {
+		if (this.r1 > r.r1) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
 }
