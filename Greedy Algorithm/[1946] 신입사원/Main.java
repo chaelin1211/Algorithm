1648e9c0fd13cee0138e8b274ee14fd8985ea98c
chaelin1211 <cofls749@naver.com>
2021-01-18 11:24:06 +0900

    Add comments

--- Greedy Algorithm/[1946] 신입사원/Main.java
+++ Greedy Algorithm/[1946] 신입사원/Main.java
@@ -8,52 +8,56 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String input = br.readLine();
-		int T = Integer.parseInt(input.split(" ")[0]);
-		for (int i = 0; i < T; i++) {
-			int N = Integer.parseInt(br.readLine());
-			ArrayList<Rank> rank = new ArrayList<Rank>();
-			for (int j = 0; j < N; j++) {
-				input = br.readLine();
-				int a = Integer.parseInt(input.split(" ")[0]);
-				int b = Integer.parseInt(input.split(" ")[1]);
-				rank.add(new Rank(a, b));
-			}
-			System.out.println(function(rank));
-		}
-	}
+        String input = br.readLine();
+        int T = Integer.parseInt(input.split(" ")[0]);
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            ArrayList<Rank> rank = new ArrayList<Rank>();
+            for (int j = 0; j < N; j++) {
+                input = br.readLine();
+                int a = Integer.parseInt(input.split(" ")[0]);
+                int b = Integer.parseInt(input.split(" ")[1]);
+                rank.add(new Rank(a, b));
+            }
+            System.out.println(function(rank));
+        }
+    }
 
-	public static int function(ArrayList<Rank> ranks) {
-		// 순위대로 정렬
-		int count = 1;
-		Collections.sort(ranks);
-		int minRank = ranks.remove(0).r2;
-		for (Rank p : ranks) {
-			if (minRank > p.r2) {
-				count++;
-				minRank = p.r2;
-			}
-		}
-		return count;
-	}
+    public static int function(ArrayList<Rank> ranks) {
+        int count = 1;
+
+        // 순위대로 정렬
+        // 1차 등급은 비교할 필요 없어짐
+        Collections.sort(ranks);
+
+        int minRank = ranks.remove(0).r2;
+        for (Rank p : ranks) {
+            // 1차 순위가 높은 지원자들보다 2차 순위가 높아야 합격 가능
+            if (minRank > p.r2) {
+                count++;
+                minRank = p.r2;
+            }
+        }
+        return count;
+    }
 }
 
 class Rank implements Comparable<Rank> {
-	int r1, r2;
+    int r1, r2;
 
-	public Rank(int r1, int r2) {
-		this.r1 = r1;
-		this.r2 = r2;
-	}
+    public Rank(int r1, int r2) {
+        this.r1 = r1;
+        this.r2 = r2;
+    }
 
-	public int compareTo(Rank r) {
-		if (this.r1 > r.r1) {
-			return 1;
-		} else {
-			return -1;
-		}
-	}
+    public int compareTo(Rank r) {
+        if (this.r1 > r.r1) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
