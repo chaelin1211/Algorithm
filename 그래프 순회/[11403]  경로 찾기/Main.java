68d5484e13bbf5432495752a5d1f221588b61f26
chaelin1211 <cofls749@naver.com>
2021-01-26 22:16:10 +0900

    Add input logic

--- 그래프 순회/[11403]  경로 찾기/Main.java
+++ 그래프 순회/[11403]  경로 찾기/Main.java
@@ -1,5 +1,18 @@
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
 public class Main {
     public static void main(String[] args) {
-
+        BufferedInputStream br = new BufferedInputStream(new InputStream(System.in));
+        int N = Integer.parseInt(br.readLine());        //정점 개수
+        int[][] incidentArray = new int[N][N];
+        
+        //input incidentTable
+        for(int i=0;i<N;i++){
+            String input = br.readLine();
+            for(int j=0;j<N;j++){
+                incidentArray[i][j] = Integer.parseInt(input.split(" ")[j]);
+            }
+        }
     }
 }
