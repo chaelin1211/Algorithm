2e6a8921ad946c1a04b9284e153ed773559facf3
chaelin1211 <cofls749@naver.com>
2021-01-28 21:39:13 +0900

    Create function using BFS

--- 그래프 순회/[11403]  경로 찾기/Main.java
+++ 그래프 순회/[11403]  경로 찾기/Main.java
@@ -1,12 +1,14 @@
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
         BufferedInputStream br = new BufferedInputStream(new InputStream(System.in));
         int N = Integer.parseInt(br.readLine()); // 정점 개수
         Vertex[] Vers = new Vertex[N];
+        int[][] possiblePath = new int[N][N];
 
         // input incidentTable
         // set vertexes
@@ -18,23 +20,47 @@ public class Main {
                 }
             }
         }
+
+        possiblePath = function(vers, N);
+    }
+
+    public int[][] function(Vertex[] vers, int N) {
+        int[][] possiblePath = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Vertex ver = vers[i];
+
+            Queue<Vertex> incidentList = new LinkedList<Vertex>();
+            incidentList.addAll(ver.getIncidentList());
+            while (!incidentList.isEmpty()) {
+                Vertex tmpVer = incidentList.remove(0);
+
+                // 중복 순회 방지
+                if (possiblePath[i][tmpVer.getI()] == 0) {
+                    possiblePath[i][tmpVer.getI()] = 1;
+                    possiblePath[tmpVer.getI()][i] = 1;
+                    queue.addAll(tmpVer.getIncidentList());
+                }
+            }
+
+        }
+        return possiblePath;
     }
 }
 
 class Vertex {
     private int i;
-    private ArrayList<Vertex> incidentVertex;
+    private Queue<Vertex> incidentVertex;
 
     public Vertex(int i) {
         this.i = i;
-        this.incidentVertex = new ArrrayList<Vertex>();
+        this.incidentVertex = new LinkedList<Vertex>();
     }
 
     public int getI() {
         return this.i;
     }
 
-    public ArrayList<Vertex> getIncidentList() {
+    public Queue<Vertex> getIncidentList() {
         return this.incidentVertex;
     }
 
