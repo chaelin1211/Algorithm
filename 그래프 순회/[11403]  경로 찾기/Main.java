9a531a2dcc4596025c0f10d91db6bd336e526742
chaelin1211 <cofls749@naver.com>
2021-01-30 22:33:54 +0900

    Add comments

--- 그래프 순회/[11403]  경로 찾기/Main.java
+++ 그래프 순회/[11403]  경로 찾기/Main.java
@@ -16,7 +16,7 @@ public class Main {
 		}
 
 		// input incidentTable
-		// set vertexes 
+		// set vertexes
 		for (int i = 0; i < N; i++) {
 			String input = br.readLine();
 			for (int j = 0; j < N; j++) {
@@ -28,40 +28,48 @@ public class Main {
 		}
 		printArray(function(Vers, N));
 	}
+
 	public static void printArray(int[][] Arr) {
-		for(int[] arr:Arr) {
-			for(int i:arr) {
-				System.out.print(i+" ");
+		for (int[] arr : Arr) {
+			for (int i : arr) {
+				System.out.print(i + " ");
 			}
 			System.out.println();
 		}
 	}
+
 	public static int[][] function(Vertex[] vers, int N) {
 		int[][] possiblePath = new int[N][N];
-		int i=0;
-		//DFS 방식
+		int i = 0;
+		// DFS 방식
 		Vertex ver = null;
 		Queue<Edge> queue = new LinkedList<Edge>();
-		while(i < N) {
-			if(queue.isEmpty()) {
-				if(i>=N) {
+		while (i < N) {
+			if (queue.isEmpty()) {
+				if (i >= N) {
 					break;
 				}
 				ver = vers[i++];
 				queue.addAll(ver.getIncidentList());
 			}
 			Edge tmpEdge = queue.remove();
-			if(tmpEdge.getFlag()) {
+			if (tmpEdge.getFlag()) {
 				continue;
 			}
 			tmpEdge.setFlag(true);
 			Vertex tmpVer = tmpEdge.getVer2();
+
+			// 1-2-3-4라고 할 때 현재 Vertex가 3이고 시작이 1일때
+			// 시작 Vertex에서 현재 Vertex 가능 표시
+			// 1에서 3까지 갈 수 있다고 표시
 			if (possiblePath[ver.getI()][tmpVer.getI()] == 0) {
 				possiblePath[ver.getI()][tmpVer.getI()] = 1;
 			}
 			queue.addAll(tmpVer.getIncidentList());
+
+			// 2->3도 표시 (나중에 중복으로 경유하는 것 방지)
 			Vertex[] verArr = tmpEdge.getVertex();
-			possiblePath[verArr[0].getI()][verArr[1].getI()] = 1;	
+			possiblePath[verArr[0].getI()][verArr[1].getI()] = 1;
 		}
 		return possiblePath;
 	}
@@ -88,40 +96,45 @@ class Vertex {
 		this.incidentEdge.add(edg);
 	}
 }
-class Edge{
+
+class Edge {
 	// 방향 그래프 ver1 -> ver2
 	private Vertex ver1, ver2;
 	private boolean flag;
-	public Edge(Vertex ver1, Vertex ver2){
+
+	public Edge(Vertex ver1, Vertex ver2) {
 		this.ver1 = ver1;
 		this.ver2 = ver2;
 		this.flag = false;
 	}
-	
+
 	public Vertex getVertex(Vertex ver) {
-		if(ver == this.ver1) {
+		if (ver == this.ver1) {
 			return this.ver2;
-		}else if(ver == this.ver2) {
+		} else if (ver == this.ver2) {
 			return this.ver1;
-		}else return null;
+		} else
+			return null;
 	}
 
 	public boolean getFlag() {
 		return this.flag;
 	}
+
 	public Vertex getVer1() {
 		return this.ver1;
 	}
+
 	public Vertex getVer2() {
 		return this.ver2;
 	}
-	
+
 	public void setFlag(boolean fl) {
 		this.flag = fl;
 	}
-	
+
 	public Vertex[] getVertex() {
-		Vertex[] verArr = {this.ver1, this.ver2};
+		Vertex[] verArr = { this.ver1, this.ver2 };
 		return verArr;
 	}
 }
