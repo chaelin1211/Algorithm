4f1bfa196dd8317f9238643135a3255751c42b15
chaelin1211 <cofls749@naver.com>
2021-01-31 15:08:42 +0900

    Add comments

--- 그래프 순회/[11403]  경로 찾기/Main.java
+++ 그래프 순회/[11403]  경로 찾기/Main.java
@@ -12,8 +12,10 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()); // 정점 개수
 		HashMap<Integer, ArrayList<Integer>> incidentHashMap = new HashMap<Integer, ArrayList<Integer>>();
+		// key: 정점 번호
+		// value: 인접 정점 (진출간선에 의해 도달할 수 있는 간선)
 
-		// input incidentTable
+		// input incidentTable----------------------------
 		for (int i = 0; i < N; i++) {
 			String input = br.readLine();
 			incidentHashMap.put(i, new ArrayList<Integer>());
@@ -23,7 +25,11 @@ public class Main {
 				}
 			}
 		}
+		// ------------------------------------------------
+
+		// function and print ----------------------------
 		printArray(function(incidentHashMap, N));
+		// ------------------------------------------------
 	}
 
 	public static void printArray(int[][] Arr) {
@@ -37,13 +43,18 @@ public class Main {
 
 	public static int[][] function(HashMap<Integer, ArrayList<Integer>> incidentHashMap, int N) {
 		int[][] possiblePath = new int[N][N];
-		int i = -1;
-		// DFS 방식
+		int i = -1; // 시작점을 가리키는 인덱스
+		// BFS 방식으로 순회
+
+		// 진출할 수 있는 정점들을 저장하는 큐
 		Queue<Integer> queue = new LinkedList<Integer>();
+
 		while (i <= N) {
 			if (queue.isEmpty()) {
 				i++;
 				ArrayList tmpArrayList = incidentHashMap.get(i);
+
+				// 인접 정점이 없는 경우 (진출 간선이 없는 경우)
 				if (tmpArrayList == null || tmpArrayList.size() == 0) {
 					continue;
 				} else {
@@ -52,11 +63,11 @@ public class Main {
 			}
 			int index = queue.remove();
 
-			if (index == i) {
+			if (index == i) {	//사이클을 통해 시작점으로 돌아옴
 				possiblePath[i][i] = 1;
 				continue;
 			} else {
-				if (possiblePath[i][index] == 0) {
+				if (possiblePath[i][index] == 0) {		// 중복 순회를 방지
 					possiblePath[i][index] = 1;
 					queue.addAll(incidentHashMap.get(index));
 				}
