2d2824f95207cebeba20ee4364449a3b1ec681c6
chaelin1211 <cofls749@naver.com>
2021-02-02 20:00:02 +0900

    Create input field

--- Basic Math/[1193] 분수찾기/Main.java
+++ Basic Math/[1193] 분수찾기/Main.java
@@ -1,5 +1,15 @@
+import java.io.BufferedReader;
+
+import jdk.internal.org.jline.utils.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // X(1 ≤ X ≤ 10,000,000)
+        int x;
+        x = Integer.parseInt(br.readLine());
+
         
     }
 }
