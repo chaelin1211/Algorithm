6764faec27fe9d4a2a870663113fb83b3b51be74
chaelin1211 <cofls749@naver.com>
2021-02-03 23:50:56 +0900

    Create a flag variable representing zigzag

--- Basic Math/[1193] 분수찾기/Main.java
+++ Basic Math/[1193] 분수찾기/Main.java
@@ -2,40 +2,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // X(1 ≤ X ≤ 10,000,000)
-        int x;
-        x = Integer.parseInt(br.readLine());
-        // 1 1/1
-        // 1+2 3 2/1
-        // 1+2+3 6 3/1
-        // 1+2+3+4 10 4/1
-        System.out.println(findNumber(x));
-    }
+		// X(1 ≤ X ≤ 10,000,000)
+		int x;
+		x = Integer.parseInt(br.readLine());
+		// 1 1/1
+		// 1+2 3 2/1
+		// 1+2+3 6 3/1
+		// 1+2+3+4 10 4/1
+		System.out.println(findNumber(x));
+	}
 
-    public static String findNumber(int x) {
-        int i = 1, j = 2;
-        int A =0, B=0;
-        while (i < x) {
-            i += j;
-            j++;
-        }
-        A = j+1;
-        B = 1;
-        int tmp;
-        tmp = A;
-        A = B;
-        B = tmp;
+	public static String findNumber(int x) {
+		int i = 1, j = 1;
+		int A = 0, B = 0;
+		int flag = -1;
 
-        while(i<x){
-            i++;
-            A++;
-            B--;
-        }
-        return Integer.toString(A)+"/"+Integer.toString(B);
-    }
+		while (i + j < x) {
+			j++;
+			i += j;
+			flag *= -1;
+		}
+
+		if (flag == 1) {
+			A = j;
+			B = 1;
+		} else {
+			A = 1;
+			B = j;
+		}
+
+		while (i < x) {
+			i++;
+			A += flag;
+			B -= flag;
+			if (A <= 0) {
+				A = 1;
+				flag *= -1;
+			}
+			if (B <= 0) {
+				B = 1;
+				flag *= -1;
+			}
+		}
+		// 1267
+
+		return Integer.toString(A) + "/" + Integer.toString(B);
+	}
 }
