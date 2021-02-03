958839dfd0e19aab9e57a5d1741f4681b517d938
chaelin1211 <cofls749@naver.com>
2021-02-04 07:16:37 +0900

    create function

--- Basic Math/[10757] 큰 수 A+B/Main.c
+++ Basic Math/[10757] 큰 수 A+B/Main.c
@@ -2,40 +2,52 @@
 #include<stdlib.h>
 int main(void) {
 	char input[2][10001];
+	scanf("%s %s", input[0], input[1]);
+    function(input[0], input[1]);
+}
+void function(char*input1, char*input2){
 	int i, j, k, tmp=0;
 
 	int answer[20001];
 	int a, b;
-	scanf("%s %s", input[0], input[1]);
 
-	i = strlen(input[0])-1;
-	j = strlen(input[1])-1;
+	i = strlen(input1)-1;
+	j = strlen(input2)-1;
 	k = 0;
+
+    // 뒤쪽(작은 쪽)부터 더한다.
 	while (i >= 0 && j >= 0) {
-		a = input[0][i--] + 1 - '1';
-		b = input[1][j--] + 1 - '1';
+		a = input1[i--] + 1 - '1';    
+		b = input2[j--] + 1 - '1';
 		tmp += a + b;
 
+        // 더해서 10이 넘을 경우를 대비
 		answer[k++] = tmp % 10;
+
+        // 10이 넘으면 다음 자릿수에 1을 더해야 함
 		tmp /= 10;
 	}	
+
 	while (i >= 0) {
-		a = input[0][i--] + 1 - '1';
+		a = input1[i--] + 1 - '1';    
 		tmp += a;
 		answer[k++] = tmp % 10;
 		tmp /= 10;
 	}
 	while (j >= 0) {
-		b = input[1][j--] + 1 - '1';
+		b = input2[j--] + 1 - '1';
 		tmp += b;
 		answer[k++] = tmp % 10;
 		tmp /= 10;
 	}
+    
 	while (tmp != 0) {
 		answer[k++] = tmp % 10;
 		tmp /= 10;
 	}
-	k--;
+	
+    // k에서 1을 빼야 마지막 값이 저장된 위치
+    k--;
 	for (; k >= 0; k--) {
 		printf("%d", answer[k]);
 	}
