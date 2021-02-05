0952baa0bed8bf18558e6a63eee5276dc6fa74b3
chaelin1211 <cofls749@naver.com>
2021-02-06 00:38:01 +0900

    Add another logic

--- Basic Math/[10250] ACM 호텔/Main.java
+++ Basic Math/[10250] ACM 호텔/Main.java
@@ -27,10 +27,33 @@ public class Main {
         }
         
         // 저층부터 고층 순으로 엘리베이터에 가까운 방을 배정하기 때문에
-        // N번 째 배정될 때 N = aH + b라면 b층의 a번 째 방에 배정되는 것이다.
+        // N번 째 배정될 때 N = aH + b라면 b층의 a+1번 째 방에 배정되는 것이다.
 
         roomNumber += (N * 100);
         roomNumber += i;
         return roomNumber;
     }
+    // public static int function(int H, int W, int N) {
+	// 	int roomNumber = 0;
+
+	// 	int a, b;
+
+	// 	// 저층부터 고층 순으로 엘리베이터에 가까운 방을 배정하기 때문에
+	// 	// N번 째 배정될 때 N = aH + b라면 b층의 a+1번 째 방에 배정되는 것이다.
+
+	// 	a = N / H;
+	// 	a++;
+		
+	// 	b = N % H;
+
+	// 	// b는 무조건 1 이상이어야 한다.
+	// 	// N = H인 경우에 b가 0이 될 수 있으므로 a에서 1을 빼고 b에 H를 더하므로써 해결 
+	// 	if(b == 0) {
+	// 		a--;
+	// 		b += H;
+	// 	}
+
+	// 	roomNumber = (b * 100) + a;
+	// 	return roomNumber;
+	// }
 }
