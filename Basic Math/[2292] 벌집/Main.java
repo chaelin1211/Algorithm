6c8edc34141e9faadf1b127b53b500d28ca4f394
chaelin1211 <cofls749@naver.com>
2021-02-05 22:40:00 +0900

    Add comments

--- Basic Math/[2292] 벌집/Main.java
+++ Basic Math/[2292] 벌집/Main.java
@@ -11,16 +11,24 @@ public class Main {
     }
 
     public static int function(int N) {
-        int level = 1;
+        // input: 방의 번호
+        // output: 1번 방으로부터 거리
+        int level = 1; // 1번방을 중심으로 주위 방은 같은 level
+                       // 같은 level의 방은 1번 방까지의 거리가 같다
         if (N == 1) {
             return level;
         }
 
-        int i = 2;
+        level += 1; // level은 1번 방을 포함해서 계산한다.
+
+        int i = 2; // i는 각 level에서 가장 작은 방의 번호이다.
+                   // level이 x일 때, 방의 개수는 6*(x-1)개이다. (육각형이기 때문!)
+                   // 즉 각 level의 방 번호 범위는? i <= N < i+((level-1)*6)
         while (true) {
-            if (i <= N && N < (i += (level * 6))) {
+            if (i <= N && N < i + ((level - 1) * 6)) {
                 return level;
             } else {
+                i += ((level - 1) * 6);
                 level++;
             }
         }
