2c7423d9fd1ff6efd66aa20cf2c3d4c39c8ce5f3
chaelin1211 <cofls749@naver.com>
2021-02-05 22:29:55 +0900

    Add exception thows declaration

--- Basic Math/[2292] 벌집/Main.java
+++ Basic Math/[2292] 벌집/Main.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
-
-import jdk.internal.org.jline.utils.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        System.out.println(funtion(N));
+        System.out.println(function(N));
     }
 
     public static int function(int N) {
