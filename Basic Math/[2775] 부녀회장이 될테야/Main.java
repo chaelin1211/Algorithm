917b05ef3b3a9d3053ad8973b642a44458b69a46
chaelin1211 <cofls749@naver.com>
2021-02-08 06:58:30 +0900

    Modify the function to be recursive

--- Basic Math/[2775] 부녀회장이 될테야/Main.java
+++ Basic Math/[2775] 부녀회장이 될테야/Main.java
@@ -6,34 +6,49 @@ public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int k, n;
 
+        int[][] testCase = new int[T][2];
+        int maxK = 0, maxN = 0;
+        int k, n;
         for (int i = 0; i < T; i++) {
             k = Integer.parseInt(br.readLine());
             n = Integer.parseInt(br.readLine());
 
-            System.out.println(function(k, n));
+            if (k > maxK) {
+                maxK = k;
+            }
+            if (n > maxN) {
+                maxN = n;
+            }
+
+            testCase[i][0] = k;
+            testCase[i][1] = n;
+        }
+        int[][] resid = new int[maxK + 1][maxN + 1];
+        for (int i = 0; i < T; i++) {
+            k = testCase[i][0];
+            n = testCase[i][1];
+            System.out.println(function(resid, k, n));
         }
 
     }
 
-    public static int function(int k, int n) {
+    public static int function(int [][]resident, int k, int n) {
         // “a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지
         // 사람들의 수의 합만큼 사람들을 데려와 살아야 한다”
 
         // K층 n호에 몇 명이 살고 있는지!!!
         // 0층부터 있고 각 층에는 1호부터 있다.
         // 0층의 i호에는 i명이 산다.
-        int[][] resident = new int[k+1][n+1];
-        for (int i = 0; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == 0) {
-                    resident[i][j] = j;
-                } else if (j == 1) {	// i != 0
-                    resident[i][j] = resident[i - 1][j];
-                } else {				// i != 0 && j != 0
-                    resident[i][j] = resident[i][j-1] + resident[i-1][j];
-                }
+        if(n == 0){     // 1호부터 있으니 제외
+            return 0;
+        }
+        if(resident[k][n] == 0){
+            if(k == 0){
+                resident[k][n] = n;
+            } 
+            else{
+                resident[k][n] = function(resident, k-1, n) + function(resident, k, n-1);
             }
         }
         return resident[k][n];
