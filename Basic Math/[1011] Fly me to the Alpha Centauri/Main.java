0be1df68b28d195a28393c23d3d8a0cd366ee2c6
chaelin1211 <cofls749@naver.com>
2021-02-13 02:29:42 +0900

    Add comments

--- Basic Math/[1011] Fly me to the Alpha Centauri/Main.java
+++ Basic Math/[1011] Fly me to the Alpha Centauri/Main.java
@@ -27,25 +27,26 @@ public class Main {
         int k = 0;
         location.add(x);
         termList.add(k);
-        
-        while (i < y) {
-            k = termList.peek();
-            term[0] = k + 1;
+
+        while (i < y) {             //i: 현재 위치
+            k = termList.peek();    // 이전 이동 범위
+            term[0] = k + 1;        // 가능한 이동 범위 세가지 모두 추출
             term[1] = k;
             term[2] = k - 1;
 
             k = -1;
-            for (int tmp : term) {
+            for (int tmp : term) {  // 이동 범위 중 가능한 범위 추출 (큰 순으로)
                 if (i + tmp < y || (i+tmp == y && tmp ==1)) {
+                                    // y를 벗어나지 않거나 y 직전일 때 이동 거리가 1인 경우 
                     k = tmp;
                     break;
                 }
             }
-            if (k <= 0) { // 가능한 이동 범위가 없음
+            if (k <= 0) {           // 가능한 이동 범위가 없음
                 termList.add(termList.pop() - 1);
-            }else if(i+k == y && k != 1){   //마지막 이동은 1이어야 한다
+            }else if(i+k == y && k != 1){   // 마지막 이동이 1이 아닌 경우
                 termList.add(termList.pop() - 1);
-            }else{
+            }else{                  // 가능한 이동 범위인 경우 stack에 저장
                 i+=k;             
                 location.add(i);
                 termList.add(k);   
