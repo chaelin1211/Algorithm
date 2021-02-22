759ac0d761397ba8bd8230fd3f9a198a991cfbe5
chaelin1211 <cofls749@naver.com>
2021-02-22 19:36:36 +0900

    Create Factorization and insert isPrimeNumber

--- Basic Math/[11653] 소인수분해/Main.java
+++ Basic Math/[11653] 소인수분해/Main.java
@@ -1,3 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Factorization(N);
+    }
+    public static void Factorization(int N){
+        if(isPrimeNumber(N)){
+            System.out.println(N);
+            return;
+        }
+        
+        int[] miniPrimes = {2, 3, 5, 7};
+        for(int x: miniPrimes){
+            while(N%x == 0){
+                N /= x;
+                System.out.println(x);
+            }
+        }
+    }
+    public static boolean isPrimeNumber(int N) {
+        if (N <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (N % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
