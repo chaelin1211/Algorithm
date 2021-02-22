46c6b075882e329d46bda456f50f1d302c0541cb
chaelin1211 <cofls749@naver.com>
2021-02-22 20:46:45 +0900

    Remove isPrimeNumber method and resize range of loop

--- Basic Math/[11653] 소인수분해/Main.java
+++ Basic Math/[11653] 소인수분해/Main.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
@@ -10,28 +11,14 @@ public class Main {
         Factorization(N);
     }
     public static void Factorization(int N){
-        if(isPrimeNumber(N)){
-            System.out.println(N);
-            return;
-        }
-        
-        int[] miniPrimes = {2, 3, 5, 7};
-        for(int x: miniPrimes){
-            while(N%x == 0){
-                N /= x;
-                System.out.println(x);
+        if(N==1) return;
+        for(int i = 2;i <= Math.sqrt(N);i++){
+            while(N % i == 0){
+                N /= i;
+                System.out.println(i);
             }
         }
-    }
-    public static boolean isPrimeNumber(int N) {
-        if (N <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(N); i++) {
-            if (N % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        if(N>1)
+        	System.out.println(N);
     }
 }
