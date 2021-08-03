65e424ff2a5a3f3a675810e264a9e27061ca2980
chaelin1211 <cofls749@naver.com>
2021-08-03 23:34:58 +0900

    새로운 풀이 추가

--- 프로그래머스/부족한 금액 계산하기/Solution.java
+++ 프로그래머스/부족한 금액 계산하기/Solution.java
@@ -7,3 +7,12 @@ class Solution {
         return answer>0 ? answer : 0;
     }
 }
+
+// class Solution   {
+//     public long solution(int price, int money, int count) {
+//         long answer = 0;
+//         answer = (long)(count * (count + 1) / 2) * price;
+//         answer -= money;
+//         return answer>0 ? answer : 0;
+//     }
+// }
